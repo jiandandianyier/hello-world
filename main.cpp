36e6da031e78136b3bd56680e0ecc1d882f140e4
夏震web <nihao_xiazhen@163.com>
2025-11-18 14:41:15 +0800

    创建main.cpp，写了初始的代码“

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#included<iostream>
+using namespace std;
+
+void  sort(int arr[],int size)
+{
+}
+
+int main()
+{
+         int arr[]={12,13,14,15,16,17};
+         int size=sizeof(arr)/sizeof(arr[0]);
+         sort(arr,size);
+         return 0;
+}
